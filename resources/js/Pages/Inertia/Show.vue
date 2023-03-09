53f6326eb63b0085c8286a4778be683320f6fb92
tona81 <t.nakashiro81@gmail.com>
2023-03-09 22:52:58 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { router } from '@inertiajs/vue3'
+
+defineProps({
+    id : String,
+    blog: Object
+})
+
+const deleteConfirm = id => {
+    // console.log(id)
+    router.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('本当に削除しますか？')
+    })
+}
+
+</script>
+
+<template>
+    {{ id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
