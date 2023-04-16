e3bf2726881b22878cac23e3de7bdfb117ddad1c
tona81 <t.nakashiro81@gmail.com>
2023-04-16 15:22:02 +0900

    sync

--- resources/js/Components/MicroModal.vue
+++ resources/js/Components/MicroModal.vue
@@ -86,6 +86,8 @@ const setCustomer = e => {
       </div>
     </div>
   </div>
-  <input name="customer" v-model="search" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-  <button @click="searchCustomers" type="button" data-micromodal-trigger="modal-1" class="flex mx-auto text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg">検索する</button>
+  <div class="flex">
+  <input name="customer" v-model="search" class="flex-1 bg-gray-100 bg-opacity-50 rounded-l border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" style="min-width: 0;">
+  <button @click="searchCustomers" type="button" data-micromodal-trigger="modal-1" class="text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded-r text-lg">検索する</button>
+</div>
 </template>
